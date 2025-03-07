28eb8cf821267c2497b74cddf5aa1b3c2ee729c7
aceesem <asemaikanatbek05@gmail.com>
2025-03-07 16:58:28 +0500

    Очистил репозиторий

--- tables/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.tables_list, name='tables_list'),
-    path('available/', views.available_tables, name='available_tables'),
-]
