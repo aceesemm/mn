9d5854296d56e5833300c21515b0b7c67147e03a
aceesem <asemaikanatbek05@gmail.com>
2025-03-07 16:56:45 +0500

    Очистил репозиторий

--- tables/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Table(models.Model):
-    number = models.IntegerField(unique=True)
-    seats = models.IntegerField(default=4)  # Указываем значение по умолчанию
-
-    def __str__(self):
-        return f"Столик {self.number} ({self.seats} мест)"
